26b68778f0094956c023f1d4a05ee8685b387098
Paul Bouchequet <paul.bouchequet@gmail.com>
2019-08-05 15:46:36 +0200

    model download

--- R/staging.R
+++ R/staging.R
@@ -7,9 +7,48 @@ score_stages <- function(signals,
     stop("Keras packagae required. Please install the Keras R package to continue: https://keras.rstudio.com/")
   }
 
-  #http://cloud.frenchkpi.com/s/fjWTZZ2omGgGa6R/download
+  model_fname <- "hd_conv_v3.h5"
+  model_f_md5 <- "5a757f2258c0675010ef617eb3e6f563"
+  model_url <- "http://cloud.frenchkpi.com/s/fjWTZZ2omGgGa6R/download"
 
+  if((!file_test("-f", model_path) && dir.exists(model_path)) |
+     (file.exists(model_path) && digest::digest(object = model_path, algo = "md5") == model_f_md5) ){
 
+    model_fullpath <- paste0(model_path,"/",model_fname)
 
+    message(paste0("Model missing or outdated. Downloading to ",model_fullpath))
 
+    if(.Platform$OS.type == "unix") {
+      if(!file.exists(model_fullpath)){
+        download.file(model_url, model_fullpath, "wget", T)
+      }
+    } else {
+      if(!file.exists(model_fullpath)){
+        download.file(model_url, model_fullpath, method = "wininet", T)
+      }
+    }
+
+  } else if (!file_test("-f", model_path) && !dir.exists(model_path)){
+
+    error("Model directory does not exist.")
+
+  } else {
+
+    model_fullpath <- model_path
+
+  }
+
+  print("Model OK")
+  print(model_fullpath)
 }
+
+train_stages <- function(){
+  "TODO"
+}
+
+build_batches <- function(){
+  "TODO"
+}
+
+
+score_stages(c(1:2),1,model_path = "./")
