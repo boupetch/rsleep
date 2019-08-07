0a09a0e0ff5ab64f45e41d1adf14bec0f964d312
Paul Bouchequet <paul.bouchequet@gmail.com>
2019-08-07 14:14:49 +0200

    automatic scoring tests

---
+++ tests/testthat/test_auto_scoring.R
@@ -0,0 +1,27 @@
+skip_if_no_keras <- function(version = NULL) {
+  if (!keras::is_keras_available(version))
+    # nocov start
+    skip("Required keras version not available for testing")
+    # nocov end
+}
+
+test_that("Test automatic scoring", {
+  skip_if_no_keras()
+
+  edf_path <- file.path(tempdir(),"15012016HD.edf")
+
+  download.file("https://osf.io/57j2u/download", edf_path, quiet = TRUE)
+
+  hypnodensity <- score_stages_edf(edf_path)
+
+  expect_equal(nrow(hypnodensity), 1472)
+  expect_equal(ncol(hypnodensity), 7)
+  expect_equal(sum(hypnodensity[1,1:5]), 1)
+
+  p <- plot_hypnodensity(hypnodensity)
+
+  expect_equal(length(p), 9)
+  expect_equal(class(p)[2], "ggplot")
+
+
+})
