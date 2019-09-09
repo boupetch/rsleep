64604064fde7a0f3bef33eac2d9cf46592aa34fd
Paul Bouchequet <paul.bouchequet@gmail.com>
2019-09-09 17:04:42 +0200

    vignettes

--- doc/Automatic_Stage_Classification.R
+++ doc/Automatic_Stage_Classification.R
@@ -10,6 +10,9 @@ knitr::opts_chunk$set(eval = all(file.exists("15012016HD.edf","15012016HD.csv"))
 ## ----setup---------------------------------------------------------------
 library(rsleep)
 
+## ----download_edf, eval=FALSE--------------------------------------------
+#  download.file("https://osf.io/57j2u/download", "15012016HD.edf")
+
 ## ------------------------------------------------------------------------
 hypnodensity <- score_stages_edf("15012016HD.edf")
 
@@ -20,6 +23,8 @@ plot_hypnodensity(hypnodensity)
 plot_hypnogram(hypnodensity)
 
 ## ---- fig.width=7--------------------------------------------------------
+download.file("https://osf.io/h4ysj/download", "15012016HD.csv")
+
 plot_hypnogram(
   read_events_noxturnal("15012016HD.csv"))
 
