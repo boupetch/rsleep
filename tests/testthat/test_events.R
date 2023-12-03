7a27d7d07cb74e8db349730c9da410181d8ddbf3
Paul Bouchequet <paul.bouchequet@gmail.com>
2023-12-03 17:06:54 +0100

    Removed test

--- tests/testthat/test_events.R
+++ tests/testthat/test_events.R
@@ -11,41 +11,41 @@ context("Creating hypnograms")
 # })
 
 test_that("Events file check", {
-
+  
   # begin column
   events <- read_events_noxturnal("data/noxturnal_events_example_unicode.csv")
   events$begin <- NULL
   expect_error(hypnogram(events))
-
+  
   # end column
   events <- read_events_noxturnal("data/noxturnal_events_example_unicode.csv")
   events$end <- NULL
   expect_error(hypnogram(events))
-
+  
   # event column
   events <- read_events_noxturnal("data/noxturnal_events_example_unicode.csv")
   events$event <- NULL
   expect_error(hypnogram(events))
-
+  
   # begin type
   events <- read_events_noxturnal("data/noxturnal_events_example_unicode.csv")
   events$begin <- as.character(events$begin)
   expect_error(hypnogram(events))
-
+  
   # end type
   events <- read_events_noxturnal("data/noxturnal_events_example_unicode.csv")
   events$end <- as.character(events$end)
   expect_error(hypnogram(events))
-
+  
   # event type
   events <- read_events_noxturnal("data/noxturnal_events_example_unicode.csv")
   events$event <- as.factor(events$event)
   expect_error(hypnogram(events))
-
+  
 })
 
 test_that("Hypnogram smoothing",{
-
+  
   hypnogram <- data.frame(
     begin = as.POSIXlt(c(1536967800,1536967830,1536967860),
                        origin = "1970-01-01"))
@@ -53,15 +53,15 @@ test_that("Hypnogram smoothing",{
                               origin = "1970-01-01")
   hypnogram$event = c("REM","N2","REM")
   
-  smooth_liang2012(hypnogram)
+  # smooth_liang2012(hypnogram)
   
-  # hypnogram <- smooth_hypnogram(hypnogram, "N2","REM", 1)
-
+  hypnogram <- smooth_hypnogram(hypnogram, "N2","REM", 1)
+  
   expect_true(all(hypnogram$event == "REM"))
-
+  
   # smooth_liang2012(
   #   read_events_noxturnal("data/noxturnal_events_example_unicode.csv"))
-
+  
 })
 
 test_that("Sleep periods",{
