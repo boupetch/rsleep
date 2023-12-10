2864f13e99eb4f78a520bad71a92c35b8ced6943
Paul Bouchequet <paul.bouchequet@gmail.com>
2023-12-10 17:12:04 +0100

    Compumedics update

--- R/files.R
+++ R/files.R
@@ -250,15 +250,18 @@ read_events_sleepedfx <- function(dir, update = TRUE){
 #' @param startTime Character string or date object of the hypnogram start.
 #' @return A dataframe of stages.
 #' @export
-read_events_compumedics <- function(txt, startTime){
+read_events_compumedics2 <- function(
+    txt, startTime, labels = c("AWA" = 0, "N1" = 1, "N2" = 2, "N3" = 3, "REM" = 5)){
   hypno <- utils::read.table(txt, stringsAsFactors = FALSE, col.names = "event")
   hypno$begin <- as.POSIXlt(startTime) + ((c(1:nrow(hypno))-1)*4)
   hypno$end <- hypno$begin+4
   hypno$event <- as.character(hypno$event)
-  hypno$event <- ifelse(hypno$event == "0","AWA",hypno$event)
-  hypno$event <- ifelse(hypno$event == "1","NREM",hypno$event)
-  hypno$event <- ifelse(hypno$event == "2","REM",hypno$event)
-  hypno$event <- ifelse(hypno$event == "?","AWA",hypno$event)
+  for(label in names(labels)){
+    hypno$event <- ifelse(hypno$event == as.character(labels[label]),label,hypno$event)
+  }
+  # hypno$event <- ifelse(hypno$event == "1","NREM",hypno$event)
+  # hypno$event <- ifelse(hypno$event == "2","REM",hypno$event)
+  # hypno$event <- ifelse(hypno$event == "?","AWA",hypno$event)
   hypno
 }
 
