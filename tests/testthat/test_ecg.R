983ce4b03d01a7408a26e0a20cce69d9f208f13b
Paul <paul.bouchequet@gmail.com>
2023-12-14 06:43:23 +0100

    check win

--- tests/testthat/test_ecg.R
+++ tests/testthat/test_ecg.R
@@ -3,7 +3,7 @@ context("ECG signal functions testing")
 test_that("Peak detection", {
   
   path <- paste0(tempdir(),"rec_1.dat")
-  download.file("https://physionet.org/files/ecgiddb/1.0.0/Person_01/rec_1.dat?download",path)
+  download.file("https://rsleep.org/data/rec_1.dat",path)
   ecg <- readBin(path,integer(),500*30)
   unlink(path)
   peaks <- detect_rpeaks(ecg, sRate = 500)
